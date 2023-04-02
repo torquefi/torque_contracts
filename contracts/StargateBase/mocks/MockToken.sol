1e0acc00496ea45ab9e8e3ebdd99d079bca43fb7
TonyDao <tuanda1@vmodev.com>
2023-04-02 20:28:22 +0700

    add stg base

---
+++ contracts/StargateBase/mocks/MockToken.sol
@@ -0,0 +1,44 @@
+// SPDX-License-Identifier: BUSL-1.1
+
+pragma solidity ^0.8.15;
+
+import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+
+contract MockToken is ERC20 {
+    constructor(string memory name_, string memory symbol_, uint8 decimals_) ERC20(name_, symbol_) {
+        newDecimals = decimals_;
+        _mint(msg.sender, 1000000000 * (10 ** decimals_));
+    }
+
+    bool paused;
+    uint8 newDecimals;
+
+    function mint(address _to, uint256 _amount) public {
+        _mint(_to, _amount);
+    }
+
+    function transfer(address recipient, uint256 amount) public virtual override returns (bool) {
+        // need to mock some failed transfer events
+        require(!paused, "Failed transfer due to pause");
+
+        return super.transfer(recipient, amount);
+    }
+
+    function transferFrom(
+        address sender,
+        address recipient,
+        uint256 amount
+    ) public virtual override returns (bool) {
+        // need to mock some failed transfer events
+        require(!paused, "Failed transfer due to pause");
+        return super.transferFrom(sender, recipient, amount);
+    }
+
+    function pauseTransfers(bool _paused) external {
+        paused = _paused;
+    }
+
+    function decimals() public view override returns (uint8) {
+        return newDecimals;
+    }
+}
