b444708939de75ef5caa1317dc35c9554cef1364
nocountryforusreal <121844155+nocountryforusreal@users.noreply.github.com>
2023-04-05 16:52:21 +0700

    draft

--- contracts/TorqueProtocol/Borrow.sol
+++ contracts/TorqueProtocol/Borrow.sol
@@ -2,10 +2,10 @@
 pragma solidity ^0.8.6;
 import "../CompoundBase/IWETH9.sol";
 import "../CompoundBase/bulkers/IBucker.sol";
-import "../CompoundBase/Comet.sol";
+import "../CompoundBase/IComet.sol";
 import "@openzeppelin/contracts/security/ReentrancyGuard.sol";
-import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 import "@openzeppelin/contracts/utils/math/SafeMath.sol";
+import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 
 contract Borrow is ReentrancyGuard{
     using SafeMath for uint256;
@@ -21,6 +21,7 @@ contract Borrow is ReentrancyGuard{
     uint public constant ACTION_WITHDRAW_ETH = 5;
     uint public constant ACTION_CLAIM_REWARD = 6;
     uint constant BASE_ASSET_MANTISA = 1e6;
+    uint constant PRICE_MANTISA = 1e2;
     uint constant SCALE = 1e18;
 
     struct BorrowInfo {
@@ -50,19 +51,86 @@ contract Borrow is ReentrancyGuard{
     function setComet(address _comet) public {
         comet = _comet;
     }
+    function allow(address _asset, address spender, uint amount) public{
+        ERC20(_asset).approve(spender, amount);
+    }
 // End test
 
-    function borrow(uint amount, uint amountBorrow) public payable nonReentrant{
-        Comet comet = Comet(comet);
+    function getBorrowable(uint amount, uint amountBorrow) public view{
+        IComet icomet = IComet(comet);
 
-        AssetInfo info = comet.getAssetInfo(0);
-        uint price = comet.getPrice(info.priceFeed);
-        uint maxBorrow = amount.mul(info.borrowCollateralFactor).mul(price).div(SCALE);
+        AssetInfo memory info = icomet.getAssetInfo(0);
+        uint price = icomet.getPrice(info.priceFeed);
+        uint maxBorrow = amount.mul(info.borrowCollateralFactor).mul(price).div(PRICE_MANTISA).div(SCALE);
+    }
+    function borrow1(uint amount, uint amountBorrow) public payable nonReentrant{
+        IComet icomet = IComet(comet);
 
-        BorrowInfo userBorrowInfo = borrowInfoMap[msg.sender];
+        AssetInfo memory info = icomet.getAssetInfo(0);
+        uint price = icomet.getPrice(info.priceFeed);
+        uint maxBorrow = amount.mul(info.borrowCollateralFactor).mul(price).div(PRICE_MANTISA).div(SCALE);
+
+        BorrowInfo storage userBorrowInfo = borrowInfoMap[msg.sender];
         uint borrowable = maxBorrow.sub(userBorrowInfo.borrowed);
-        require(borrowable > amount, "borrow exceed");
-        require(ERC20(token_).transferFrom(msg.sender,address(this), amount), "transfer asset fail");
+        require(borrowable >= amount, "borrow exceed");
+        ERC20(asset).approve(comet, amount);
+    }
+    function borrow2(uint amount, uint amountBorrow) public payable nonReentrant{
+        IComet icomet = IComet(comet);
+
+        AssetInfo memory info = icomet.getAssetInfo(0);
+        uint price = icomet.getPrice(info.priceFeed);
+        uint maxBorrow = amount.mul(info.borrowCollateralFactor).mul(price).div(PRICE_MANTISA).div(SCALE);
+
+        BorrowInfo storage userBorrowInfo = borrowInfoMap[msg.sender];
+        uint borrowable = maxBorrow.sub(userBorrowInfo.borrowed);
+        require(borrowable >= amount, "borrow exceed");
+        ERC20(asset).approve(comet, amount);
+        require(ERC20(asset).transferFrom(msg.sender,address(this), amount), "transfer asset fail");
+
+        userBorrowInfo.borrowed += amount;
+        userBorrowInfo.supplied += amount;
+    }
+    function borrow3(uint amount, uint amountBorrow) public payable nonReentrant{
+        IComet icomet = IComet(comet);
+
+        AssetInfo memory info = icomet.getAssetInfo(0);
+        uint price = icomet.getPrice(info.priceFeed);
+        uint maxBorrow = amount.mul(info.borrowCollateralFactor).mul(price).div(PRICE_MANTISA).div(SCALE);
+
+        BorrowInfo storage userBorrowInfo = borrowInfoMap[msg.sender];
+        uint borrowable = maxBorrow.sub(userBorrowInfo.borrowed);
+        require(borrowable >= amount, "borrow exceed");
+        ERC20(asset).approve(comet, amount);
+        require(ERC20(asset).transferFrom(msg.sender,address(this), amount), "transfer asset fail");
+
+        userBorrowInfo.borrowed += amount;
+        userBorrowInfo.supplied += amount;
+        uint[] memory actions = new uint[](2);
+
+        actions[0] = ACTION_SUPPLY_ASSET;
+        actions[1] = ACTION_WITHDRAW_ASSET;
+
+        bytes[] memory callData = new bytes[](2);
+
+        bytes memory supplyAssetCalldata = abi.encode(comet, address(this), asset, amount);
+        callData[0] = supplyAssetCalldata;
+
+        bytes memory withdrawAssetCalldata = abi.encode(comet, msg.sender, usdc, amountBorrow);
+        callData[1] = withdrawAssetCalldata;
+    }
+    function borrow(uint amount, uint amountBorrow) public payable nonReentrant{
+        IComet icomet = IComet(comet);
+
+        AssetInfo memory info = icomet.getAssetInfo(0);
+        uint price = icomet.getPrice(info.priceFeed);
+        uint maxBorrow = amount.mul(info.borrowCollateralFactor).mul(price).div(PRICE_MANTISA).div(SCALE);
+
+        BorrowInfo storage userBorrowInfo = borrowInfoMap[msg.sender];
+        uint borrowable = maxBorrow.sub(userBorrowInfo.borrowed);
+        require(borrowable >= amount, "borrow exceed");
+        ERC20(asset).approve(comet, amount);
+        require(ERC20(asset).transferFrom(msg.sender,address(this), amount), "transfer asset fail");
 
         userBorrowInfo.borrowed += amount;
         userBorrowInfo.supplied += amount;
@@ -85,11 +153,37 @@ contract Borrow is ReentrancyGuard{
     }
 
 
+    function repay1(uint amount, uint amountClaim) public payable nonReentrant{
+        BorrowInfo storage userBorrowInfo = borrowInfoMap[msg.sender];
+        require(userBorrowInfo.borrowed <= amount, "borrow exceed");
+        require(userBorrowInfo.supplied >= amountClaim, "supply exceed");
+        require(ERC20(usdc).transferFrom(msg.sender,address(this), amount), "transfer asset fail");
+    }
+    function repay2(uint amount, uint amountClaim) public payable nonReentrant{
+        BorrowInfo storage userBorrowInfo = borrowInfoMap[msg.sender];
+        require(userBorrowInfo.borrowed <= amount, "borrow exceed");
+        require(userBorrowInfo.supplied >= amountClaim, "supply exceed");
+        require(ERC20(usdc).transferFrom(msg.sender,address(this), amount), "transfer asset fail");
+
+        userBorrowInfo.borrowed = userBorrowInfo.borrowed.sub(amount);
+        userBorrowInfo.supplied = userBorrowInfo.supplied.sub(amountClaim);
+
+        uint[] memory actions = new uint[](2);
+
+        actions[0] = ACTION_SUPPLY_ASSET;
+        actions[1] = ACTION_WITHDRAW_ASSET;
+
+        bytes[] memory callData = new bytes[](2);
+
+        bytes memory supplyAssetCalldata = abi.encode(comet, address(this), usdc, amount);
+        callData[0] = supplyAssetCalldata;
+
+        bytes memory withdrawAssetCalldata = abi.encode(comet, msg.sender, usdc, amountClaim);
+        callData[1] = withdrawAssetCalldata;
+
+    }
     function repay(uint amount, uint amountClaim) public payable nonReentrant{
-
-        Comet comet = Comet(comet);
-
-        BorrowInfo userBorrowInfo = borrowInfoMap[msg.sender];
+        BorrowInfo storage userBorrowInfo = borrowInfoMap[msg.sender];
         require(userBorrowInfo.borrowed <= amount, "borrow exceed");
         require(userBorrowInfo.supplied >= amountClaim, "supply exceed");
         require(ERC20(usdc).transferFrom(msg.sender,address(this), amount), "transfer asset fail");
