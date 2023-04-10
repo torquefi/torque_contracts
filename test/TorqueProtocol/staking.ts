d82f7b072f64e9aa3a1b603ce933fad56dab9dc9
Anh Tuan <tuanda1@vmodev.com>
2023-04-10 11:43:41 +0700

    create mock up swap for test

--- test/TorqueProtocol/staking.ts
+++ test/TorqueProtocol/staking.ts
@@ -3,7 +3,7 @@ import { anyValue } from "@nomicfoundation/hardhat-chai-matchers/withArgs";
 import { expect } from "chai";
 import { ethers, network } from "hardhat";
 
-describe("Lock", function () {
+describe("Staking Contracts", function () {
   // We define a fixture to reuse the same setup in every test.
   // We use loadFixture to run this setup once, snapshot that state,
   // and reset Hardhat Network to that snapshot in every test.
