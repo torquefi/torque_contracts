2a9107a58ec1d8b508b10553a413bb8917b60e46
nocountryforusreal <121844155+nocountryforusreal@users.noreply.github.com>
2023-10-14 08:49:04 +0700

    add usdc fund

---
+++ contracts/TorqueProtocol/ARBI_CompoundFund.sol
@@ -0,0 +1,52 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.6;
+import "../CompoundBase/IComet.sol";
+import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+import "@openzeppelin/contracts-upgradeable/token/ERC20/ERC20Upgradeable.sol";
+import "@openzeppelin/contracts-upgradeable/proxy/utils/UUPSUpgradeable.sol";
+import "@openzeppelin/contracts-upgradeable/access/OwnableUpgradeable.sol";
+import "@openzeppelin/contracts-upgradeable/security/ReentrancyGuardUpgradeable.sol";
+
+contract CompoundFund  is UUPSUpgradeable, OwnableUpgradeable, ReentrancyGuardUpgradeable{
+
+    address public baseAsset;
+    address public comet;
+
+    
+    mapping(address => bool) private _governor;
+
+    modifier onlyGovernor() {
+        require(
+            owner() == msg.sender || _governor[msg.sender] == true,
+            "Caller is not the governor"
+        );
+        _;
+    }
+    
+    function _authorizeUpgrade(address) internal override onlyOwner {}
+
+    function initialize(address _comet, address _baseAsset) public initializer {
+        comet = _comet;
+        baseAsset = _baseAsset;
+        __Ownable_init();
+        __ReentrancyGuard_init();
+    }
+
+    function setGovernor(address governor, bool active) public onlyOwner() {
+        _governor[governor] = active;
+    }
+
+    function deposit(uint amount) public nonReentrant() onlyGovernor(){
+        require(ERC20(baseAsset).transferFrom(msg.sender, address(this), amount), "transfer token fail");
+        ERC20(baseAsset).approve(comet, amount);
+        IComet(comet).supply(baseAsset, amount);
+    } 
+    function withdraw(uint withdrawAmount) public nonReentrant() onlyGovernor(){
+        require(withdrawAmount <= IComet(comet).balanceOf(address(this)), "exceed fund's balance");
+        IComet(comet).withdraw(baseAsset, withdrawAmount);
+        require(ERC20(baseAsset).transfer(msg.sender, withdrawAmount), "transfer token fail");
+    } 
+    function totalAsset() public view returns (uint256) {
+        return IComet(comet).balanceOf(address(this));
+    }
+}
