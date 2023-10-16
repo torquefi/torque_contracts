6346a07413f6e50c329da08f6302e0282ad20e63
nocountryforusreal <121844155+nocountryforusreal@users.noreply.github.com>
2023-10-16 10:00:28 +0700

    refactor package

--- contracts/TorqueProtocol/Torque/ARBI_CompoundFund.sol
+++ contracts/TorqueProtocol/Torque/ARBI_CompoundFund.sol
@@ -1,6 +1,6 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.6;
-import "../CompoundBase/IComet.sol";
+import "../../CompoundBase/IComet.sol";
 import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 import "@openzeppelin/contracts-upgradeable/token/ERC20/ERC20Upgradeable.sol";
 import "@openzeppelin/contracts-upgradeable/proxy/utils/UUPSUpgradeable.sol";
