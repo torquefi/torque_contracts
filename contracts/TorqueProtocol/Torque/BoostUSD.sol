aed991308f4d9c9867478b0b7c4b13d5d83cedbe
AnhTuan <anhtuanpcipho@gmail.com>
2023-10-19 07:50:54 +0700

    init boost usd

---
+++ contracts/TorqueProtocol/Torque/BoostUSD.sol
@@ -0,0 +1,218 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.15;
+
+import "@openzeppelin/contracts/access/Ownable.sol";
+import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
+import "@openzeppelin/contracts/utils/math/SafeMath.sol";
+import "../Interfaces/IStargateLPStaking.sol";
+// import "../Interfaces/ISwapRouter.sol"; // upgrade to Uniswap V3
+import "../Interfaces/ISwapRouterV3.sol";
+import "../Interfaces/IWETH.sol";
+
+import "../Interfaces/IGMX.sol";
+
+/**
+
+********\                                                
+\__**  __|                                               
+   ** | ******\   ******\   ******\  **\   **\  ******\  
+   ** |**  __**\ **  __**\ **  __**\ ** |  ** |**  __**\ 
+   ** |** /  ** |** |  \__|** /  ** |** |  ** |******** |
+   ** |** |  ** |** |      ** |  ** |** |  ** |**   ____|
+   ** |\******  |** |      \******* |\******  |\*******\ 
+   \__| \______/ \__|       \____** | \______/  \_______|
+                                 ** |                    
+                                 ** |                    
+                                 \__|                    
+
+ */
+
+contract BoostUSD is Ownable {
+    using SafeMath for uint256;
+    // variables and mapping
+    ISwapRouterV3 public swapRouter;
+    IGMX public gmxInterface;
+    address constant WETH = 0xB4FBF271143F4FBf7B91A5ded31805e42b2208d6;
+    mapping(address => uint256) public totalStack;
+    address treasuryWallet;
+    address usdAddress;
+    address usdcAddress;
+    uint256 treasuryProportion;
+    uint256 constant DENOMINATOR = 10000;
+    // address[] public stakeHolders;
+
+    mapping(address => UserInfo) public userInfo;
+    mapping(address => mapping(uint256 => bool)) public isStakeHolder;
+    address[] public stakeHolders;
+
+    // structs and events
+    struct UserInfo {
+        uint256 amount;
+        uint256 amountToCompound;
+        uint256 amountFromUniswap;
+        uint256 lastProcess;
+    }
+
+    // constructor and functions
+    constructor(
+        address _swapRouter,
+        address _treasuryWallet,
+        uint256 _treasuryProportion,
+        address _usdAddress,
+        address _usdcAddress
+    ) {
+        swapRouter = ISwapRouterV3(_swapRouter);
+        treasuryWallet = _treasuryWallet;
+        treasuryProportion = _treasuryProportion;
+        usdAddress = _usdAddress;
+        usdcAddress = _usdcAddress;
+    }
+
+    function changeConfigAddress(address _swapRouter) public onlyOwner {
+        swapRouter = ISwapRouterV3(_swapRouter);
+    }
+
+    function deposit(address _token, uint256 _amount) public payable {
+        UserInfo storage _userInfo = userInfo[_msgSender()];
+        IERC20 tokenInterface = IERC20(_token);
+        tokenInterface.transferFrom(_msgSender(), address(this), _amount);
+        uint256 usdToConvert = _amount.mul(3).div(4);
+        ISwapRouterV3.ExactInputSingleParams memory params = ISwapRouterV3.ExactInputSingleParams({
+            tokenIn: usdAddress,
+            tokenOut: usdcAddress,
+            fee: 10000,
+            recipient: address(this),
+            deadline: block.timestamp + 1000000,
+            amountIn: usdToConvert,
+            amountOutMinimum: 0,
+            sqrtPriceLimitX96: 0
+        });
+        uint256 usdcAmount = swapRouter.exactInputSingle(params);
+        // TO-DO
+        // Add liquidity to Uniswap V3
+
+        // Add collateral to Compound
+
+        if (_userInfo.lastProcess == 0) {
+            address[] storage stakes = stakeHolders;
+            stakes.push(_msgSender());
+        }
+        _userInfo.amount = _userInfo.amount.add(_amount);
+        _userInfo.amountToSTG = _userInfo.amountToSTG.add(_amount / 2);
+        _userInfo.amountFromGMX = _userInfo.amountFromGMX.add(gmTokenAmount);
+        _userInfo.lastProcess = block.timestamp;
+        totalStack[_token] = totalStack[_token].add(_amount);
+    }
+
+    function withdraw(address _token, uint256 _amount) public payable {
+        uint256 pid = addressToPid[_token];
+        UserInfo storage _userInfo = userInfo[_msgSender()][pid];
+        uint256 currentAmount = _userInfo.amount;
+        _userInfo.amount = currentAmount.sub(_amount);
+        uint256 amountFromSTG = _userInfo.amountToSTG.mul(_amount).div(currentAmount);
+        uint256 amountToGMX = _userInfo.amountFromGMX.mul(_amount).div(currentAmount);
+        _userInfo.lastProcess = block.timestamp;
+        totalStack[_token] = totalStack[_token].sub(_amount);
+
+        IERC20 tokenInterface = IERC20(_token);
+        lpStaking.withdraw(pid, amountFromSTG);
+        uint256 tokenFromGMX = gmxInterface.createWithdrawal(amountToGMX);
+        uint256 totalTokenReturn = tokenFromGMX + amountFromSTG;
+        if (_token == WETH) {
+            IWETH weth = IWETH(WETH);
+            weth.withdraw(amountFromSTG);
+            (bool success, ) = msg.sender.call{ value: totalTokenReturn }("");
+            require(success, "Transfer ETH failed");
+        } else {
+            tokenInterface.transfer(_msgSender(), _amount);
+        }
+    }
+
+    function autoCompound(address _token) public {
+        IERC20 tokenInterface = IERC20(_token);
+        uint256 pid = addressToPid[_token];
+        uint256 totalProduction = calculateTotalProduct(pid);
+        lpStaking.withdraw(pid, totalStack[_token]);
+        uint256 rewardSTG = stargateInterface.balanceOf(address(this));
+        if (rewardSTG > 0) {
+            uint256 tokenReward = swapRewardSTGToToken(_token, rewardSTG);
+            uint256 treasuryReserved = tokenReward.mul(treasuryProportion).div(DENOMINATOR);
+            uint256 remainReward = tokenReward.sub(treasuryReserved);
+            tokenInterface.transfer(treasuryWallet, treasuryReserved);
+            address[] memory stakes = stakeHolders[pid];
+            for (uint256 i = 0; i < stakes.length; i++) {
+                UserInfo storage _userInfo = userInfo[stakes[i]][pid];
+                uint256 userProduct = calculateUserProduct(pid, stakes[i]);
+                uint256 reward = remainReward.mul(userProduct).div(totalProduction);
+                _userInfo.amount = _userInfo.amount.add(reward);
+            }
+            totalStack[_token] = totalStack[_token].add(remainReward);
+            tokenInterface.approve(address(lpStaking), totalStack[_token]);
+            lpStaking.deposit(pid, totalStack[_token]);
+        }
+    }
+
+    // internal functions
+    function calculateTotalProduct(uint256 _pid) internal view returns (uint256) {
+        address[] memory stakes = stakeHolders[_pid];
+        uint256 totalProduct = 0;
+        for (uint256 i = 0; i < stakes.length; i++) {
+            uint256 userProduct = calculateUserProduct(_pid, stakes[i]);
+            totalProduct = totalProduct.add(userProduct);
+        }
+        return totalProduct;
+    }
+
+    function calculateUserProduct(uint256 _pid, address _staker) internal view returns (uint256) {
+        UserInfo memory _userInfo = userInfo[_staker][_pid];
+        uint256 interval = block.timestamp.sub(_userInfo.lastProcess);
+        return interval.mul(_userInfo.amount);
+    }
+
+    function swapRewardSTGToToken(
+        address _token,
+        uint256 _stgAmount
+    ) internal returns (uint256 amountOut) {
+        stargateInterface.approve(address(swapRouter), _stgAmount);
+        ISwapRouterV3.ExactInputSingleParams memory params = ISwapRouterV3.ExactInputSingleParams({
+            tokenIn: address(stargateInterface),
+            tokenOut: _token,
+            fee: 10000,
+            recipient: address(this),
+            deadline: block.timestamp + 1000000,
+            amountIn: _stgAmount,
+            amountOutMinimum: 0,
+            sqrtPriceLimitX96: 0
+        });
+        amountOut = swapRouter.exactInputSingle(params);
+    }
+
+    function updateTreasury(address _treasuryWallet) public onlyOwner {
+        treasuryWallet = _treasuryWallet;
+    }
+
+    function updateTreasuryProportion(uint256 _treasuryProportion) public onlyOwner {
+        require(_treasuryProportion < DENOMINATOR, "Invalid treasury proportion");
+        treasuryProportion = _treasuryProportion;
+    }
+
+    // For test
+    function swapETHToSTG() public payable {
+        IWETH weth = IWETH(WETH);
+        uint256 _amount = msg.value;
+        weth.deposit{ value: _amount }();
+        IERC20 tokenInterface = IERC20(WETH);
+        tokenInterface.approve(address(swapRouter), _amount);
+        ISwapRouterV3.ExactInputSingleParams memory params = ISwapRouterV3.ExactInputSingleParams({
+            tokenIn: WETH,
+            tokenOut: address(stargateInterface),
+            fee: 10000,
+            recipient: msg.sender,
+            deadline: block.timestamp + 1000000,
+            amountIn: _amount,
+            amountOutMinimum: 0,
+            sqrtPriceLimitX96: 0
+        });
+        swapRouter.exactInputSingle(params);
+    }
+}
