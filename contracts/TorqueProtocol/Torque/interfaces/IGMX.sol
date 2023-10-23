ed5d5361d5bceade8d31c1135dec5b4085fe02f4
wormbsc <camconrad@gmail.com>
2023-10-23 00:32:08 -0700

    refactor repo

---
+++ contracts/TorqueProtocol/Torque/interfaces/IGMX.sol
@@ -0,0 +1,16 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.13;
+
+//  _________  ________  ________  ________  ___  ___  _______      
+// |\___   ___\\   __  \|\   __  \|\   __  \|\  \|\  \|\  ___ \     
+// \|___ \  \_\ \  \|\  \ \  \|\  \ \  \|\  \ \  \\\  \ \   __/|    
+//     \ \  \ \ \  \\\  \ \   _  _\ \  \\\  \ \  \\\  \ \  \_|/__  
+//      \ \  \ \ \  \\\  \ \  \\  \\ \  \\\  \ \  \\\  \ \  \_|\ \ 
+//       \ \__\ \ \_______\ \__\\ _\\ \_____  \ \_______\ \_______\
+//        \|__|  \|_______|\|__|\|__|\|___| \__\|_______|\|_______|
+
+interface IGMX {
+    function createDeposit(uint256 _amount) external payable returns (uint256);
+
+    function createWithdrawal(uint256 _amount) external payable returns (uint256);
+}
---
+++ contracts/TorqueProtocol/Torque/interfaces/INonfungiblePositionManager.sol
@@ -0,0 +1,66 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.13;
+
+//  _________  ________  ________  ________  ___  ___  _______      
+// |\___   ___\\   __  \|\   __  \|\   __  \|\  \|\  \|\  ___ \     
+// \|___ \  \_\ \  \|\  \ \  \|\  \ \  \|\  \ \  \\\  \ \   __/|    
+//     \ \  \ \ \  \\\  \ \   _  _\ \  \\\  \ \  \\\  \ \  \_|/__  
+//      \ \  \ \ \  \\\  \ \  \\  \\ \  \\\  \ \  \\\  \ \  \_|\ \ 
+//       \ \__\ \ \_______\ \__\\ _\\ \_____  \ \_______\ \_______\
+//        \|__|  \|_______|\|__|\|__|\|___| \__\|_______|\|_______|
+
+interface INonfungiblePositionManager {
+    struct MintParams {
+        address token0;
+        address token1;
+        uint24 fee;
+        int24 tickLower;
+        int24 tickUpper;
+        uint amount0Desired;
+        uint amount1Desired;
+        uint amount0Min;
+        uint amount1Min;
+        address recipient;
+        uint deadline;
+    }
+
+    function mint(
+        MintParams calldata params
+    ) external payable returns (uint tokenId, uint128 liquidity, uint amount0, uint amount1);
+
+    struct IncreaseLiquidityParams {
+        uint tokenId;
+        uint amount0Desired;
+        uint amount1Desired;
+        uint amount0Min;
+        uint amount1Min;
+        uint deadline;
+    }
+
+    function increaseLiquidity(
+        IncreaseLiquidityParams calldata params
+    ) external payable returns (uint128 liquidity, uint amount0, uint amount1);
+
+    struct DecreaseLiquidityParams {
+        uint tokenId;
+        uint128 liquidity;
+        uint amount0Min;
+        uint amount1Min;
+        uint deadline;
+    }
+
+    function decreaseLiquidity(
+        DecreaseLiquidityParams calldata params
+    ) external payable returns (uint amount0, uint amount1);
+
+    struct CollectParams {
+        uint tokenId;
+        address recipient;
+        uint128 amount0Max;
+        uint128 amount1Max;
+    }
+
+    function collect(
+        CollectParams calldata params
+    ) external payable returns (uint amount0, uint amount1);
+}
