95059f9485afedcfec502e75405999a69ef5b809
wormbsc <camconrad@gmail.com>
2023-10-22 23:58:20 -0700

    push farm and refine update

---
+++ contracts/TorqueProtocol/USD/libraries/IncentiveId.sol
@@ -0,0 +1,22 @@
+// SPDX-License-Identifier: GPL-2.0-or-later
+pragma solidity =0.7.6;
+pragma abicoder v2;
+
+//  _________  ________  ________  ________  ___  ___  _______      
+// |\___   ___\\   __  \|\   __  \|\   __  \|\  \|\  \|\  ___ \     
+// \|___ \  \_\ \  \|\  \ \  \|\  \ \  \|\  \ \  \\\  \ \   __/|    
+//     \ \  \ \ \  \\\  \ \   _  _\ \  \\\  \ \  \\\  \ \  \_|/__  
+//      \ \  \ \ \  \\\  \ \  \\  \\ \  \\\  \ \  \\\  \ \  \_|\ \ 
+//       \ \__\ \ \_______\ \__\\ _\\ \_____  \ \_______\ \_______\
+//        \|__|  \|_______|\|__|\|__|\|___| \__\|_______|\|_______|
+
+import '../interfaces/IUniswapV3Staker.sol';
+
+library IncentiveId {
+    /// @notice Calculate the key for a staking incentive
+    /// @param key The components used to compute the incentive identifier
+    /// @return incentiveId The identifier for the incentive
+    function compute(IUniswapV3Staker.IncentiveKey memory key) internal pure returns (bytes32 incentiveId) {
+        return keccak256(abi.encode(key));
+    }
+}
