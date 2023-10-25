c7b333f60cdbde2bfec145c3735065033228909a
wormbsc <camconrad@gmail.com>
2023-10-24 22:33:50 -0700

    refine repo

--- test/USD.ts
+++ test/USD.ts
@@ -6,7 +6,7 @@ import {
   import { expect } from "chai";
   import { ethers } from "hardhat";
   
-  describe("# Tokenized USD by Torque Inc.", function () {
+  describe("Torque USD", function () {
     // We define a fixture to reuse the same setup in every test.
     // We use loadFixture to run this setup once, snapshot that state,
     // and reset Hardhat Network to that snapshot in every test.
