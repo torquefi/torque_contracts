c566157fb1990e967addd1d5dd099e6f303d5d7b
wormbsc <camconrad@gmail.com>
2023-11-09 12:27:05 -0800

    prep contracts for tony

---
+++ contracts/TorqueProtocol/Torque/vaults/CompoundUSD.sol
@@ -0,0 +1,28 @@
+pragma solidity ^0.8.0;
+
+//  _________  ________  ________  ________  ___  ___  _______
+// |\___   ___\\   __  \|\   __  \|\   __  \|\  \|\  \|\  ___ \
+// \|___ \  \_\ \  \|\  \ \  \|\  \ \  \|\  \ \  \\\  \ \   __/|
+//     \ \  \ \ \  \\\  \ \   _  _\ \  \\\  \ \  \\\  \ \  \_|/__
+//      \ \  \ \ \  \\\  \ \  \\  \\ \  \\\  \ \  \\\  \ \  \_|\ \
+//       \ \__\ \ \_______\ \__\\ _\\ \_____  \ \_______\ \_______\
+//        \|__|  \|_______|\|__|\|__|\|___| \__\|_______|\|_______|
+
+/// @dev Must import Compound interfaces
+
+import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
+import "@openzeppelin/contracts/token/ERC20/utils/SafeERC20.sol";
+import "@openzeppelin/contracts/security/ReentrancyGuard.sol";
+import "@openzeppelin/contracts/access/Ownable.sol";
+import "@openzeppelin/contracts/token/ERC20/extensions/ERC4626.sol";
+
+import "./../vToken.sol";
+
+contract CompoundUSD is ERC4626, Ownable, ReentrancyGuard {
+    using SafeERC20 for IERC20;
+
+    // Logic to supply leftover USDC (50%) to Compound for USDC interest and COMP rewards
+    // Logic for auto-compounding COMP for more USDC and resupplying USDC to Compound
+    // Logic to direct performance fee to treasury with 12 hr min duration between calls
+
+}
