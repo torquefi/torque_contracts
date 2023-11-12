a66c9bfece88dbdb1b66820831760502b7a68135
AnhTuan <anhtuanpcipho@gmail.com>
2023-11-12 12:56:18 +0700

    feat: finish integrate gmx protocol

---
+++ contracts/TorqueProtocol/Torque/interfaces/IGMXV2ETH.sol
@@ -0,0 +1,49 @@
+pragma solidity ^0.8.0;
+
+//  _________  ________  ________  ________  ___  ___  _______
+// |\___   ___\\   __  \|\   __  \|\   __  \|\  \|\  \|\  ___ \
+// \|___ \  \_\ \  \|\  \ \  \|\  \ \  \|\  \ \  \\\  \ \   __/|
+//     \ \  \ \ \  \\\  \ \   _  _\ \  \\\  \ \  \\\  \ \  \_|/__
+//      \ \  \ \ \  \\\  \ \  \\  \\ \  \\\  \ \  \\\  \ \  \_|\ \
+//       \ \__\ \ \_______\ \__\\ _\\ \_____  \ \_______\ \_______\
+//        \|__|  \|_______|\|__|\|__|\|___| \__\|_______|\|_______|
+
+interface IGMXV2ETH {
+    struct CreateDepositParams {
+        address receiver;
+        address callbackContract;
+        address uiFeeReceiver;
+        address market;
+        address initialLongToken;
+        address initialShortToken;
+        address[] longTokenSwapPath;
+        address[] shortTokenSwapPath;
+        uint256 minMarketTokens;
+        bool shouldUnwrapNativeToken;
+        uint256 executionFee;
+        uint256 callbackGasLimit;
+    }
+
+    struct CreateWithdrawalParams {
+        address receiver;
+        address callbackContract;
+        address uiFeeReceiver;
+        address market;
+        address[] longTokenSwapPath;
+        address[] shortTokenSwapPath;
+        uint256 marketTokenAmount;
+        uint256 minLongTokenAmount;
+        uint256 minShortTokenAmount;
+        bool shouldUnwrapNativeToken;
+        uint256 executionFee;
+        uint256 callbackGasLimit;
+    }
+
+    function deposit(
+        CreateWithdrawalParams calldata params
+    ) external returns (uint256 gmTokenAmount);
+
+    function withdraw(
+        CreateWithdrawalParams calldata params
+    ) external returns (uint256 wethAmount, uint256 usdcAmount);
+}
--- contracts/TorqueProtocol/Torque/vaults/StargateETH.sol
+++ contracts/TorqueProtocol/Torque/vaults/StargateETH.sol
@@ -1,15 +1,15 @@
 pragma solidity ^0.8.0;
 
-//  _________  ________  ________  ________  ___  ___  _______      
-// |\___   ___\\   __  \|\   __  \|\   __  \|\  \|\  \|\  ___ \     
-// \|___ \  \_\ \  \|\  \ \  \|\  \ \  \|\  \ \  \\\  \ \   __/|    
-//     \ \  \ \ \  \\\  \ \   _  _\ \  \\\  \ \  \\\  \ \  \_|/__  
-//      \ \  \ \ \  \\\  \ \  \\  \\ \  \\\  \ \  \\\  \ \  \_|\ \ 
+//  _________  ________  ________  ________  ___  ___  _______
+// |\___   ___\\   __  \|\   __  \|\   __  \|\  \|\  \|\  ___ \
+// \|___ \  \_\ \  \|\  \ \  \|\  \ \  \|\  \ \  \\\  \ \   __/|
+//     \ \  \ \ \  \\\  \ \   _  _\ \  \\\  \ \  \\\  \ \  \_|/__
+//      \ \  \ \ \  \\\  \ \  \\  \\ \  \\\  \ \  \\\  \ \  \_|\ \
 //       \ \__\ \ \_______\ \__\\ _\\ \_____  \ \_______\ \_______\
 //        \|__|  \|_______|\|__|\|__|\|___| \__\|_______|\|_______|
 
 import "../interfaces/ISwapRouterV3.sol";
-import "../interfaces/IStargateLPStaking.sol";
+import "./../interfaces/IStargateLPStaking.sol";
 
 import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
 import "@openzeppelin/contracts/token/ERC20/utils/SafeERC20.sol";
@@ -22,25 +22,29 @@ contract StargateETH is Ownable, ReentrancyGuard {
     using SafeERC20 for IERC20;
 
     IERC20 public immutable weth;
-    IStargate public immutable stargatePool;
-    TorqueETH public immutable torqueETHInstance;
+    IStargateLPStaking lpStaking;
 
-    constructor(IERC20 _weth, IStargate _stargatePool, address torqueETHAddress) {
+    mapping(address => uint256) public addressToPid;
+
+    constructor(IERC20 _weth, IStargate _stargatePool, address _stargateStakingAddress,) {
         weth = _weth;
-        stargatePool = _stargatePool;
-        torqueETHInstance = TorqueETH(torqueETHAddress);
+       lpStaking = IStargateLPStaking(_stargateStakingAddress);
     }
 
-    function deposit(uint256 _amount) external nonReentrant {
+    function setPid(address _token, uint256 _pid) public onlyOwner {
+        addressToPid[_token] = _pid;
+    }
+
+    function _depositStargate(address _token, uint256 _amount) external nonReentrant {
+        uint256 pid = addressToPid[_token];
         weth.safeTransferFrom(msg.sender, address(this), _amount);
         weth.approve(address(stargatePool), _amount);
-        stargatePool.deposit(_amount);
-        torqueETHInstance.mint(msg.sender, _amount);
+       lpStaking.deposit(pid, _amount);
     }
 
-    function withdraw(uint256 _amount) external nonReentrant {
-        stargatePool.withdraw(_amount);
+    function _withdrawStargate(address _token, uint256 _amount) external nonReentrant {
+        uint256 pid = addressToPid[_token];
         weth.safeTransfer(msg.sender, _amount);
-        torqueETHInstance.burn(msg.sender, _amount);
+        lpStaking.withdraw(pid, _amount);
     }
 }
