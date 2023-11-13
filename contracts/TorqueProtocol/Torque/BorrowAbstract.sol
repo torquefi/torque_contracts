a45991a6c88eadda5cf9d274bdee852825d53eee
nocountryforusreal <121844155+nocountryforusreal@users.noreply.github.com>
2023-11-13 17:13:59 +0700

    refactor cuz hardhat not support external lib link

---
+++ contracts/TorqueProtocol/Torque/BorrowAbstract.sol
@@ -0,0 +1,222 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.6;
+
+//  _________  ________  ________  ________  ___  ___  _______
+// |\___   ___\\   __  \|\   __  \|\   __  \|\  \|\  \|\  ___ \
+// \|___ \  \_\ \  \|\  \ \  \|\  \ \  \|\  \ \  \\\  \ \   __/|
+//     \ \  \ \ \  \\\  \ \   _  _\ \  \\\  \ \  \\\  \ \  \_|/__
+//      \ \  \ \ \  \\\  \ \  \\  \\ \  \\\  \ \  \\\  \ \  \_|\ \
+//       \ \__\ \ \_______\ \__\\ _\\ \_____  \ \_______\ \_______\
+//        \|__|  \|_______|\|__|\|__|\|___| \__\|_______|\|_______|
+
+
+import "./RewardUtil.sol";
+import "../../CompoundBase/IWETH9.sol";
+import "../../CompoundBase/bulkers/IBulker.sol";
+import "../../CompoundBase/IComet.sol";
+
+import "./interfaces/ICometRewards.sol";
+import "./interfaces/IUSDEngine.sol";
+
+
+import "@openzeppelin/contracts/utils/math/SafeMath.sol";
+import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+import "@openzeppelin/contracts-upgradeable/token/ERC20/ERC20Upgradeable.sol";
+import "@openzeppelin/contracts-upgradeable/proxy/utils/UUPSUpgradeable.sol";
+import "@openzeppelin/contracts-upgradeable/access/OwnableUpgradeable.sol";
+import "@openzeppelin/contracts-upgradeable/security/ReentrancyGuardUpgradeable.sol";
+
+abstract contract BorrowAbstract is UUPSUpgradeable, OwnableUpgradeable, ReentrancyGuardUpgradeable {
+    using SafeMath for uint256;
+    
+    /// @notice The action for supplying an asset to Comet
+    bytes32 public constant ACTION_SUPPLY_ASSET = "ACTION_SUPPLY_ASSET";
+
+    /// @notice The action for supplying a native asset (e.g. ETH on Ethereum mainnet) to Comet
+    bytes32 public constant ACTION_SUPPLY_ETH = "ACTION_SUPPLY_NATIVE_TOKEN";
+
+    /// @notice The action for transferring an asset within Comet
+    bytes32 public constant ACTION_TRANSFER_ASSET = "ACTION_TRANSFER_ASSET";
+
+    /// @notice The action for withdrawing an asset from Comet
+    bytes32 public constant ACTION_WITHDRAW_ASSET = "ACTION_WITHDRAW_ASSET";
+
+    /// @notice The action for withdrawing a native asset from Comet
+    bytes32 public constant ACTION_WITHDRAW_ETH = "ACTION_WITHDRAW_NATIVE_TOKEN";
+
+    /// @notice The action for claiming rewards from the Comet rewards contract
+    bytes32 public constant ACTION_CLAIM_REWARD = "ACTION_CLAIM_REWARD";
+
+    address public bulker;
+    address public asset;
+    address public baseAsset;
+    address public comet;
+    address public cometReward;
+    address public engine;
+    address public usd;
+    address public rewardUtil;
+    address public rewardToken;
+    address public treasury;
+    uint public lastClaimCometTime;
+    uint public claimPeriod;
+    
+    uint constant BASE_ASSET_MANTISA = 1e6;
+    uint constant PRICE_MANTISA = 1e2;
+    uint constant SCALE = 1e18;
+    uint constant WITHDRAW_OFFSET = 1e2;
+    uint constant USD_DECIMAL_OFFSET = 1e12;
+
+    struct BorrowInfo {
+        address user;
+        uint baseBorrowed;
+        uint borrowed;
+        uint supplied;
+
+        uint borrowTime;
+        uint reward;
+    }
+
+    mapping(address => BorrowInfo) public borrowInfoMap;
+    event UserBorrow(address user, address collateralAddress, uint amount);
+    event UserRepay(address user, address collateralAddress, uint repayAmount, uint claimAmount);
+    
+    function _authorizeUpgrade(address) internal override onlyOwner {}
+
+    function initialize(address _comet, address _cometReward, address _asset, address _baseAsset, address _bulker, address _engine, address _usd, address _treasury, address _rewardUtil, address _rewardToken) public initializer {
+        comet = _comet;
+        cometReward = _cometReward;
+        asset = _asset;
+        baseAsset = _baseAsset;
+        bulker = _bulker;
+        engine = _engine;
+        usd = _usd;
+        treasury = _treasury;
+        rewardUtil = _rewardUtil;
+        rewardToken = _rewardToken;
+        IComet(comet).allow(_bulker, true);
+        claimPeriod = 86400;
+        __Ownable_init();
+        __ReentrancyGuard_init();
+    }
+    // Test only
+    function setBulker(address _bulker) public onlyOwner{
+        bulker = _bulker;
+    }
+    function setasset(address _asset) public onlyOwner{
+        asset = payable(_asset);
+    }
+    
+    function setComet(address _comet) public onlyOwner{
+        comet = _comet;
+    }
+    function allow(address _asset, address spender, uint amount) public onlyOwner{
+        ERC20(_asset).approve(spender, amount);
+    }
+    function setAllowTo(address manager, bool _allow) public onlyOwner{
+        IComet(comet).allow(manager, _allow);
+    }
+
+    function setUsdEngine(address _newEngine) public onlyOwner{
+        engine = _newEngine;
+    }
+
+    function setUsd(address _usd) public onlyOwner{
+        usd = _usd;
+    }
+    // End test
+
+    // Gets max amount that can be borrowed by user
+    function getBorrowable(uint amount) public view returns (uint){
+        IComet icomet = IComet(comet);
+
+        AssetInfo memory info = icomet.getAssetInfoByAddress(asset);
+        uint price = icomet.getPrice(info.priceFeed);
+        return amount.mul(info.borrowCollateralFactor).mul(price).div(PRICE_MANTISA).div(SCALE);
+    }
+    // Allows a user to withdraw their collateral
+    function withdraw(uint withdrawAmount) public nonReentrant(){
+        
+        // Fetch a users borrowing information
+        BorrowInfo storage userBorrowInfo = borrowInfoMap[msg.sender];
+        require(userBorrowInfo.supplied > 0, "User does not have asset");
+        
+        if(userBorrowInfo.borrowed > 0) {
+            uint reward = RewardUtil(rewardUtil).calculateReward(userBorrowInfo.baseBorrowed, userBorrowInfo.borrowTime);
+            userBorrowInfo.reward = userBorrowInfo.reward.add(reward);
+            uint accruedInterest = calculateInterest(userBorrowInfo.borrowed, userBorrowInfo.borrowTime);
+            userBorrowInfo.borrowed = userBorrowInfo.borrowed.add(accruedInterest);
+            userBorrowInfo.borrowTime = block.timestamp;
+        }
+
+        IComet icomet = IComet(comet);
+
+        AssetInfo memory info = icomet.getAssetInfoByAddress(asset);
+        uint price = icomet.getPrice(info.priceFeed);
+
+        uint minRequireSupplyAmount = userBorrowInfo.borrowed.mul(SCALE).mul(PRICE_MANTISA).div(price).div(uint(info.borrowCollateralFactor).sub(WITHDRAW_OFFSET));
+        uint withdrawableAmount = userBorrowInfo.supplied - minRequireSupplyAmount;
+
+        require(withdrawAmount < withdrawableAmount, "Exceeds asset supply");
+
+        userBorrowInfo.supplied = userBorrowInfo.supplied.sub(withdrawAmount);
+
+        bytes[] memory callData = new bytes[](1);
+
+        bytes memory withdrawAssetCalldata = abi.encode(comet, address(this), asset, withdrawAmount);
+        callData[0] = withdrawAssetCalldata;
+
+        IBulker(bulker).invoke(buildWithdraw(), callData);
+
+        ERC20(asset).transfer(msg.sender, withdrawAmount);
+    } 
+    
+   
+    
+    function borrowBalanceOf(address user) public view returns (uint) {
+        
+        BorrowInfo storage userBorrowInfo = borrowInfoMap[user];
+        if(userBorrowInfo.borrowed == 0) {
+            return 0;
+        }
+
+        uint borrowAmount = userBorrowInfo.borrowed;
+        uint interest = calculateInterest(borrowAmount, userBorrowInfo.borrowTime);
+
+        return borrowAmount + interest;
+    }
+
+    function calculateInterest(uint borrowAmount, uint borrowTime) public view returns (uint) {
+        IComet icomet = IComet(comet);
+        uint totalSecond = block.timestamp - borrowTime;
+        return borrowAmount.mul(icomet.getBorrowRate(icomet.getUtilization())).mul(totalSecond).div(1e18);
+    }
+
+    function claimCReward() public onlyOwner{
+        require(lastClaimCometTime + claimPeriod < block.timestamp, "already claim");
+        require(treasury != address(0), "invalid treasury");
+        lastClaimCometTime = block.timestamp;
+        ICometRewards(cometReward).claim(comet, treasury, true);
+    }
+
+    receive() external payable {
+    }
+
+    function buildBorrowAction() pure public returns(bytes32[] memory) {
+        bytes32[] memory actions = new bytes32[](2);
+        actions[0] = ACTION_SUPPLY_ASSET;
+        actions[1] = ACTION_WITHDRAW_ASSET;
+        return actions;
+    }
+    function buildWithdraw() pure public returns(bytes32[] memory) {
+        bytes32[] memory actions = new bytes32[](1);
+        actions[0] = ACTION_WITHDRAW_ASSET;
+        return actions;
+    }
+    function buildRepay() pure public returns(bytes32[] memory) {
+        bytes32[] memory actions = new bytes32[](2);
+
+        actions[0] = ACTION_SUPPLY_ASSET;
+        actions[1] = ACTION_WITHDRAW_ASSET;
+        return actions;
+    }
+}
--- scripts/deploy_eth_eborrow.ts
+++ scripts/deploy_eth_eborrow.ts
@@ -2,7 +2,8 @@ import { ethers } from "hardhat";
 
 async function main() {
   const Borrow = await ethers.getContractFactory("ETHBorrow");
-  const borrow = await upgrades.deployProxy(Borrow, ['0x3EE77595A8459e93C2888b13aDB354017B198188','0xef9e070044d62C38D2e316146dDe92AD02CF2c2c','0x42a71137C09AE83D8d05974960fd607d40033499','0x07865c6E87B9F70255377e024ace6630C1Eaa37F', '0xf82AAB8ae0E7F6a2ecBfe2375841d83AeA4cb9cE', '0x5c51Fb12f845569369A838e2c6868Cb06d8b35De', '0x2098025567E0511Ea7db7F369347bcD29B8DFB30', '0x0f773B3d518d0885DbF0ae304D87a718F68EEED5','0xa6e8bAf56d88CbD6cC4f238D1443A852109d548d','0x07865c6E87B9F70255377e024ace6630C1Eaa37F'], {
+  //_comet, _cometReward, _asset, _baseAsset, bulker,  _engine,  _usd,  _treasury,  _rewardUtil,  _rewardToken
+  const borrow = await upgrades.deployProxy(Borrow, ['0x9c4ec768c28520B50860ea7a15bd7213a9fF58bf','0x88730d254A2f7e6AC8388c3198aFd694bA9f7fae','0x82af49447d8a07e3bd95bd0d56f35241523fbab1','0xaf88d065e77c8cC2239327C5EDb3A432268e5831', '0xbdE8F31D2DdDA895264e27DD990faB3DC87b372d', '0x3c716812B02aC5Ea432b153B134770e7f78E6542', '0xB50B92Fa490AA2366751F04b52C5f3350AD4AC16',  '0x10Df08c7265EBBa82Bd0619aA3f2C2B5621e140C','0xba32a1F10Be022210d9EBC7FFa191a7023cb3601','0xaf88d065e77c8cC2239327C5EDb3A432268e5831'], {
     initializer: "initialize",
   });
   const borrowResult = await borrow.deployed();
