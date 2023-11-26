a6fe0155c2665847b2c91a1864e2c6739f4b4dfd
AnhTuan <anhtuanpcipho@gmail.com>
2023-11-26 18:26:12 +0700

    feature: finish refactor and boost contract

--- contracts/TorqueProtocol/Torque/CompoundFund.sol
+++ contracts/TorqueProtocol/Torque/CompoundFund.sol
@@ -1,11 +1,11 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.6;
 
-//  _________  ________  ________  ________  ___  ___  _______      
-// |\___   ___\\   __  \|\   __  \|\   __  \|\  \|\  \|\  ___ \     
-// \|___ \  \_\ \  \|\  \ \  \|\  \ \  \|\  \ \  \\\  \ \   __/|    
-//     \ \  \ \ \  \\\  \ \   _  _\ \  \\\  \ \  \\\  \ \  \_|/__  
-//      \ \  \ \ \  \\\  \ \  \\  \\ \  \\\  \ \  \\\  \ \  \_|\ \ 
+//  _________  ________  ________  ________  ___  ___  _______
+// |\___   ___\\   __  \|\   __  \|\   __  \|\  \|\  \|\  ___ \
+// \|___ \  \_\ \  \|\  \ \  \|\  \ \  \|\  \ \  \\\  \ \   __/|
+//     \ \  \ \ \  \\\  \ \   _  _\ \  \\\  \ \  \\\  \ \  \_|/__
+//      \ \  \ \ \  \\\  \ \  \\  \\ \  \\\  \ \  \\\  \ \  \_|\ \
 //       \ \__\ \ \_______\ \__\\ _\\ \_____  \ \_______\ \_______\
 //        \|__|  \|_______|\|__|\|__|\|___| \__\|_______|\|_______|
 
@@ -17,11 +17,10 @@ import "@openzeppelin/contracts-upgradeable/proxy/utils/UUPSUpgradeable.sol";
 import "@openzeppelin/contracts-upgradeable/access/OwnableUpgradeable.sol";
 import "@openzeppelin/contracts-upgradeable/security/ReentrancyGuardUpgradeable.sol";
 
-contract CompoundFund is UUPSUpgradeable, OwnableUpgradeable, ReentrancyGuardUpgradeable{
-
+contract CompoundFund is UUPSUpgradeable, OwnableUpgradeable, ReentrancyGuardUpgradeable {
     address public baseAsset;
     address public comet;
-    
+
     mapping(address => bool) private _governor;
 
     modifier onlyGovernor() {
@@ -31,7 +30,7 @@ contract CompoundFund is UUPSUpgradeable, OwnableUpgradeable, ReentrancyGuardUpg
         );
         _;
     }
-    
+
     function _authorizeUpgrade(address) internal override onlyOwner {}
 
     function initialize(address _comet, address _baseAsset) public initializer {
@@ -41,21 +40,27 @@ contract CompoundFund is UUPSUpgradeable, OwnableUpgradeable, ReentrancyGuardUpg
         __ReentrancyGuard_init();
     }
 
-    function setGovernor(address governor, bool active) public onlyOwner() {
+    function setGovernor(address governor, bool active) public onlyOwner {
         _governor[governor] = active;
     }
 
-    function deposit(uint amount) public nonReentrant() onlyGovernor(){
-        require(ERC20(baseAsset).transferFrom(msg.sender, address(this), amount), "transfer token fail");
+    function deposit(uint amount) public nonReentrant onlyGovernor {
+        require(
+            ERC20(baseAsset).transferFrom(msg.sender, address(this), amount),
+            "transfer token fail"
+        );
         ERC20(baseAsset).approve(comet, amount);
-        IComet(comet).supply(baseAsset, amount);
-    } 
-    function withdraw(uint withdrawAmount) public nonReentrant() onlyGovernor(){
-        require(withdrawAmount <= IComet(comet).balanceOf(address(this)), "exceed fund's balance");
-        IComet(comet).withdraw(baseAsset, withdrawAmount);
+        // TO-DO: Implement the correct interface
+        // IComet(comet).supply(baseAsset, amount);
+    }
+
+    function withdraw(uint withdrawAmount) public nonReentrant onlyGovernor {
+        // require(withdrawAmount <= IComet(comet).balanceOf(address(this)), "exceed fund's balance");
+        // IComet(comet).withdraw(baseAsset, withdrawAmount);
         require(ERC20(baseAsset).transfer(msg.sender, withdrawAmount), "transfer token fail");
-    } 
+    }
+
     function totalAsset() public view returns (uint256) {
-        return IComet(comet).balanceOf(address(this));
+        // return IComet(comet).balanceOf(address(this));
     }
 }
--- contracts/TorqueProtocol/Torque/RewardUtil.sol
+++ contracts/TorqueProtocol/Torque/RewardUtil.sol
@@ -1,11 +1,11 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.6;
 
-//  _________  ________  ________  ________  ___  ___  _______      
-// |\___   ___\\   __  \|\   __  \|\   __  \|\  \|\  \|\  ___ \     
-// \|___ \  \_\ \  \|\  \ \  \|\  \ \  \|\  \ \  \\\  \ \   __/|    
-//     \ \  \ \ \  \\\  \ \   _  _\ \  \\\  \ \  \\\  \ \  \_|/__  
-//      \ \  \ \ \  \\\  \ \  \\  \\ \  \\\  \ \  \\\  \ \  \_|\ \ 
+//  _________  ________  ________  ________  ___  ___  _______
+// |\___   ___\\   __  \|\   __  \|\   __  \|\  \|\  \|\  ___ \
+// \|___ \  \_\ \  \|\  \ \  \|\  \ \  \|\  \ \  \\\  \ \   __/|
+//     \ \  \ \ \  \\\  \ \   _  _\ \  \\\  \ \  \\\  \ \  \_|/__
+//      \ \  \ \ \  \\\  \ \  \\  \\ \  \\\  \ \  \\\  \ \  \_|\ \
 //       \ \__\ \ \_______\ \__\\ _\\ \_____  \ \_______\ \_______\
 //        \|__|  \|_______|\|__|\|__|\|___| \__\|_______|\|_______|
 
@@ -13,16 +13,16 @@ import "@openzeppelin/contracts/utils/math/SafeMath.sol";
 import "@openzeppelin/contracts-upgradeable/proxy/utils/UUPSUpgradeable.sol";
 import "@openzeppelin/contracts-upgradeable/access/OwnableUpgradeable.sol";
 
-contract RewardUtil  is UUPSUpgradeable, OwnableUpgradeable {
+contract RewardUtil is UUPSUpgradeable, OwnableUpgradeable {
     using SafeMath for uint256;
+
     function _authorizeUpgrade(address) internal override onlyOwner {}
 
     function initialize() public initializer {
         __Ownable_init();
     }
 
-    function calculateReward(uint _amount, uint _from) external view returns (uint){
+    function calculateReward(uint _amount, uint _from) external view returns (uint) {
         return 0;
     }
-    
 }
