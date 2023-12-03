94bb01283ce04b924991c8beb7f0917d9038aad6
AnhTuan <anhtuanpcipho@gmail.com>
2023-12-03 11:12:32 +0700

    feature: migrate all uniswap to 0.8 version

---
+++ scripts/test-boost/deploy-test-gmx.ts
@@ -0,0 +1,20 @@
+import { ethers } from "hardhat";
+
+async function main() {
+  const testStargate = await ethers.deployContract("TestStargate", [
+    "0xbf22f0f184bccbea268df387a49ff5238dd23e40", // Stargate Router ETH
+    "0x53bf833a5d6c4dda888f69c22c88c9f356a41614", // Stargate Router
+    "0xB4FBF271143F4FBf7B91A5ded31805e42b2208d6", // WETH
+  ]);
+
+  await testStargate.waitForDeployment();
+
+  console.log(`TestStargate deployed at ${testStargate.target}`);
+}
+
+// We recommend this pattern to be able to use async/await everywhere
+// and properly handle errors.
+main().catch((error) => {
+  console.error(error);
+  process.exitCode = 1;
+});
