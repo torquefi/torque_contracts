2054b1bdaa550710eaf96fff9c561f199a0c09f8
wormbsc <camconrad@gmail.com>
2023-12-18 05:42:29 -0800

    prep

---
+++ contracts/TorqueProtocol/Torque/BoostTORQ.sol
@@ -0,0 +1,33 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.15;
+
+//  _________  ________  ________  ________  ___  ___  _______
+// |\___   ___\\   __  \|\   __  \|\   __  \|\  \|\  \|\  ___ \
+// \|___ \  \_\ \  \|\  \ \  \|\  \ \  \|\  \ \  \\\  \ \   __/|
+//     \ \  \ \ \  \\\  \ \   _  _\ \  \\\  \ \  \\\  \ \  \_|/__
+//      \ \  \ \ \  \\\  \ \  \\  \\ \  \\\  \ \  \\\  \ \  \_|\ \
+//       \ \__\ \ \_______\ \__\\ _\\ \_____  \ \_______\ \_______\
+//        \|__|  \|_______|\|__|\|__|\|___| \__\|_______|\|_______|
+
+import "./interfaces/ISwapRouterV3.sol";
+import "./interfaces/INonfungiblePositionManager.sol";
+
+import "@openzeppelin/contracts/access/Ownable.sol";
+import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
+import "@openzeppelin/contracts/utils/math/SafeMath.sol";
+import "@openzeppelin/contracts/security/ReentrancyGuard.sol";
+
+import "./vaults/SushiCOMP.sol";
+import "./vaults/UniswapCOMP.sol";
+
+import "./tToken.sol";
+import "./RewardUtil";
+
+contract BoostTORQ is Ownable, ReentrancyGuard {
+    using SafeMath for uint256;
+
+    // Logic to mint and burn receipt token
+    // Logic to split deposits between child vaults
+    // Logic to manage fees and rewards
+
+}
--- contracts/TorqueProtocol/Torque/vaults/UniswapTORQ.sol
+++ contracts/TorqueProtocol/Torque/vaults/UniswapTORQ.sol
@@ -9,7 +9,7 @@ pragma solidity ^0.8.0;
 //       \ \__\ \ \_______\ \__\\ _\\ \_____  \ \_______\ \_______\
 //        \|__|  \|_______|\|__|\|__|\|___| \__\|_______|\|_______|
 
-/// @dev Must import Compound interfaces
+/// @dev Must import Uniswap interfaces
 
 import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
 import "@openzeppelin/contracts/token/ERC20/utils/SafeERC20.sol";
@@ -19,10 +19,11 @@ import "@openzeppelin/contracts/token/ERC20/extensions/ERC4626.sol";
 
 import "./../vToken.sol";
 
-contract CompoundUSD {
+contract UniswapTORQ is ERC4626, Ownable, ReentrancyGuard {
     using SafeERC20 for IERC20;
 
-    // Logic to supply leftover USDC (50%) to Compound for USDC interest and COMP rewards
-    // Logic for auto-compounding COMP for more USDC and resupplying USDC to Compound
-    // Logic to direct performance fee to treasury with 12 hr min duration between calls
+    // Logic to supply TORQ deposits to Uniswap V3 pool
+    // Logic for auto-compounding fees to grow LPs position
+    // Logic to direct performance fee to treasury
+
 }
