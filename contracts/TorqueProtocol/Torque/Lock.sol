0cff5aacc4ebfad32c6a34efa63dab38a654c43a
wormbsc <camconrad@gmail.com>
2023-12-21 04:13:28 -0800

    prep torque boost torq

--- contracts/TorqueProtocol/Torque/Lock.sol
+++ contracts/TorqueProtocol/Torque/Lock.sol
@@ -13,7 +13,7 @@ contract Lock {
     uint public unlockTime;
     address payable public owner;
 
-    event Withdrawal(uint amount, uint when);
+    event LockVerified(uint amount, uint when);
 
     constructor(uint _unlockTime) payable {
         require(block.timestamp < _unlockTime, "Unlock time should be in the future");
@@ -22,15 +22,10 @@ contract Lock {
         owner = payable(msg.sender);
     }
 
-    function withdraw() public {
-        // Uncomment this line, and the import of "hardhat/console.sol", to print a log in your terminal
-        // console.log("Unlock time is %o and block timestamp is %o", unlockTime, block.timestamp);
-
+    function verifyLock() public {
         require(block.timestamp >= unlockTime, "You can't withdraw yet");
         require(msg.sender == owner, "You aren't the owner");
-
-        emit Withdrawal(address(this).balance, block.timestamp);
-
         owner.transfer(address(this).balance);
+        emit LockVerified(address(this).balance, block.timestamp);
     }
 }
--- contracts/TorqueProtocol/Torque/vaults/UniswapTORQ.sol
+++ contracts/TorqueProtocol/Torque/vaults/UniswapTORQ.sol
@@ -21,34 +21,35 @@ import "../vToken.sol";
 contract UniswapTORQ is Ownable, ReentrancyGuard {
     using SafeERC20 for IERC20;
 
-    // Uniswap V3 position manager
-    INonfungiblePositionManager public positionManager;
+    struct Config {
+        address torqToken;
+        address wethToken;
+        address positionManager;
+        address vaultToken;
+        address treasury;
+        uint256 performanceFee;
+        uint24 poolFee;
+    }
 
-    // TORQ and WETH token contracts
-    IERC20 public torqToken;
-    IERC20 public wethToken;
+    struct State {
+        INonfungiblePositionManager positionManager;
+        IERC20 torqToken;
+        IERC20 wethToken;
+        UniswapTORQ vaultToken;
+        address treasury;
+        uint256 slippage;
+        uint24 poolFee;
+        int24 tickLower;
+        int24 tickUpper;
+        uint256 tokenId;
+        uint256 liquidity;
+    }
 
-    // vToken contract for this vault
-    UniswapTORQ public vaultToken;
+    event Deposited(uint256 amount);
+    event Withdrawal(uint256 amount);
 
-    // Treasury and performance fee
-    address public treasury;
-    uint256 public performanceFee;
-
-    // Uniswap pool details
-    uint24 public poolFee;
-    int24 public tickLower;
-    int24 public tickUpper;
-
-    // Liquidity position details
-    uint256 public tokenId;
-    uint256 public liquidity;
-
-    // Events
-    event LiquidityAdded(uint256 torqAmount, uint256 wethAmount);
-    event LiquidityRemoved(uint256 torqAmount, uint256 wethAmount);
-    event FeesCompounded();
-    event PerformanceFeeClaimed();
+    Config public config;
+    State public state;
 
     constructor(
         address _torqToken,
@@ -72,93 +73,112 @@ contract UniswapTORQ is Ownable, ReentrancyGuard {
         tickUpper = 224040;
     }
 
-    function addLiquidity(uint256 torqAmount, uint256 wethAmount) external nonReentrant {
-        torqToken.safeTransferFrom(msg.sender, address(this), torqAmount);
-        wethToken.safeTransferFrom(msg.sender, address(this), wethAmount);
-
-        torqToken.safeApprove(address(positionManager), torqAmount);
+    function deposit(uint256 amount) external nonReentrant {
+        torqToken.safeTransferFrom(msg.sender, address(this), amount);
+        uint256 torqToConvert = amount / 2; 
+        uint256 torqToKeep = amount - torqToConvert;
+        uint256 wethAmount = convertTORQtoWETH(torqToConvert);
+        torqToken.safeApprove(address(positionManager), torqToKeep);
         wethToken.safeApprove(address(positionManager), wethAmount);
-
+        uint256 amount0Min = torqToKeep * (10000 - slippage) / 10000;
+        uint256 amount1Min = wethAmount * (10000 - slippage) / 10000;
         INonfungiblePositionManager.MintParams memory params = INonfungiblePositionManager.MintParams({
             token0: address(torqToken),
             token1: address(wethToken),
             fee: poolFee,
             tickLower: tickLower,
             tickUpper: tickUpper,
-            amount0Desired: torqAmount,
+            amount0Desired: torqToKeep,
             amount1Desired: wethAmount,
-            amount0Min: 0,
-            amount1Min: 0,
+            amount0Min: amount0Min,
+            amount1Min: amount1Min,
             recipient: address(this),
-            deadline: block.timestamp + 15 minutes
+            deadline: block.timestamp + 2 minutes
         });
-
         (tokenId, liquidity,,) = positionManager.mint(params);
-
-        // Mint corresponding vUNITORQ tokens
-        vaultToken.mint(msg.sender, calculateVTokenAmount(torqAmount, wethAmount));
-
-        emit LiquidityAdded(torqAmount, wethAmount);
+        vaultToken.mint(msg.sender, calculateVTokenMintAmount(torqToKeep, wethAmount));
+        emit Deposited(torqToKeep, wethAmount);
     }
 
-    function removeLiquidity(uint256 liquidityAmount) external nonReentrant {
-        require(liquidityAmount > 0, "UniswapTORQ: Invalid liquidity amount");
-        require(liquidity >= liquidityAmount, "UniswapTORQ: Insufficient liquidity");
-
-        // Calculate minimum amounts based on max slippage of 0.5%
-        uint256 amount0Min = torqAmount - (torqAmount * 0.5 / 100);
-        uint256 amount1Min = wethAmount - (wethAmount * 0.5 / 100);
-
-        // Define txn deadline
+    function withdraw(uint256 amount) external nonReentrant {
+        require(amount > 0, "UniswapTORQ: Invalid amount");
+        require(liquidity >= amount, "UniswapTORQ: Insufficient liquidity");
+        (uint256 expectedTorqAmount, uint256 expectedWethAmount) = calculateExpectedTokenAmounts(amount);
+        uint256 amount0Min = expectedTorqAmount * (10000 - slippage) / 10000;
+        uint256 amount1Min = expectedWethAmount * (10000 - slippage) / 10000;
+        uint256 amount0Min = expectedTorqAmount - (expectedTorqAmount * 0.5 / 100);
+        uint256 amount1Min = expectedWethAmount - (expectedWethAmount * 0.5 / 100);
         uint256 deadline = block.timestamp + 2 minutes;
-
-        // Decrease liquidity
         INonfungiblePositionManager.DecreaseLiquidityParams memory decreaseLiquidityParams = INonfungiblePositionManager.DecreaseLiquidityParams({
             tokenId: tokenId,
-            liquidity: liquidityAmount,
+            liquidity: amount,
             amount0Min: amount0Min,
             amount1Min: amount1Min,
             deadline: deadline
         });
-
         (uint256 amount0, uint256 amount1) = positionManager.decreaseLiquidity(decreaseLiquidityParams);
-
-        // Collect the withdrawn assets
         INonfungiblePositionManager.CollectParams memory collectParams = INonfungiblePositionManager.CollectParams({
             tokenId: tokenId,
             recipient: address(this),
             amount0Max: uint128(amount0),
             amount1Max: uint128(amount1)
         });
-
         positionManager.collect(collectParams);
-
-        // Update internal liquidity accounting
-        liquidity -= liquidityAmount;
-
-        // Burn the corresponding vUNITORQ tokens
-        // Calculate amt of vUNITORQ tokens to burn (requires a valuation strategy).
-        uint256 burnAmount = calculateVTokenBurnAmount(liquidityAmount);
+        liquidity -= amount;
+        uint256 burnAmount = calculateVTokenBurnAmount(amount);
         vaultToken.burn(address(this), burnAmount);
-
-        // Transfer the collected TORQ and WETH back to the BoostTORQ contract
-        torqToken.safeTransfer(msg.sender, amount0); // Transfer TORQ to BoostTORQ
-        wethToken.safeTransfer(msg.sender, amount1); // Transfer WETH to BoostTORQ
-
-        emit LiquidityRemoved(torqAmount, wethAmount);
-    }
-
-    function calculateVTokenAmount(uint256 torqAmount, uint256 wethAmount) internal view returns (uint256) {
-        // Logic to calculate the amount of vUNITORQ tokens to mint based on provided liquidity
-        // This might involve a valuation strategy for the liquidity provided
-        // ...
-
-        return vTokenAmount;
+        uint256 convertedTorqAmount = convertWETHtoTORQ(amount1);
+        amount0 = amount0.add(convertedTorqAmount);
+        uint256 remainingWeth = amount1 - /* Amount of WETH converted to TORQ */;
+        torqToken.safeTransfer(msg.sender, amount0);
+        wethToken.safeTransfer(msg.sender, remainingWeth);
+        emit Withdrawal(amount0, remainingWeth);
     }
 
     function setTickRange(int24 _tickLower, int24 _tickUpper) external onlyOwner {
         require(_newLower < _newUpper, "Invalid tick range");
         tickLower = _tickLower;
         tickUpper = _tickUpper;
+    }
+
+    function setSlippage(uint256 _slippage) external onlyOwner {
+        slippage = _slippage;
+    }
+
+    function setTreasury(address _treasury) external onlyOwner {
+        treasury = _treasury;
+    }
+
+    function setPerformanceFee(uint256 _performanceFee) external onlyOwner {
+        performanceFee = _performanceFee;
+    }
+
+    function setPoolFee(uint24 _poolFee) external onlyOwner {
+        poolFee = _poolFee;
+    }
+
+    function calculateExpectedTokenAmounts(uint256 liquidityAmount) internal view returns (uint256 expectedTorqAmount, uint256 expectedWethAmount) {
+        // Calculate the expected amount of TORQ and WETH tokens to receive
+        return (calculatedTorqAmount, calculatedWethAmount);
+    }
+
+    function calculateVTokenMintAmount(uint256 torqAmount, uint256 wethAmount) internal view returns (uint256) {
+        // Calculate the amount of vUNITORQ tokens to mint
+        return vTokenAmount;
+    }
+
+    function calculateVTokenBurnAmount(uint256 liquidityAmount) internal view returns (uint256) {
+        // Calculate the amount of vUNITORQ tokens to burn
+        return vTokenAmount;
+    }
+
+    function convertTORQtoWETH(uint256 torqAmount) internal returns (uint256) {
+        // Swap TORQ for WETH
+        return wethAmount;
+    }
+
+    function convertWETHtoTORQ(uint256 wethAmount) internal returns (uint256) {
+        // Swap WETH for TORQ
+        return torqAmount;
     }
 }
