e263be42335cd41466b5300d5af5b951e6dbe91f
wormbsc <camconrad@gmail.com>
2023-12-24 19:07:50 -0800

    refine

--- contracts/TorqueProtocol/Torque/BoostBTC.sol
+++ contracts/TorqueProtocol/Torque/BoostBTC.sol
@@ -20,9 +20,119 @@ import "./strategies/UniswapBTC.sol";
 import "./tToken.sol";
 
 contract BoostBTC is BoostAbstract {
+    using SafeMath for uint256;
+    using SafeERC20 for IERC20;
 
-    // Logic to mint and burn receipt token
-    // Logic to split deposits between child vaults
-    // Logic to manage fees and rewards
+    struct Config {
+        address treasury;
+        uint256 gmxV2btcPercent;
+        uint256 uniswapbtcPercent;
+        uint256 performanceFee;
+    }
 
+    struct Addresses {
+        address tTokenContract;
+        address wbtcTokenAddress;
+        address gmxV2btcVaultAddress;
+        address uniswapbtcVaultAddress;
+        address treasury;
+    }
+
+    Config public config;
+    Addresses public addresses;
+    Iwbtc public wbtcToken;
+    tToken public tTokenContract;
+    GMXV2Btc public gmxV2btcVault;
+    uniswapbtc public uniswapbtcVault;
+    RewardUtil public rewardUtil;
+    uint public totalSupplied;
+    uint256 public lastCompoundTimestamp;
+
+    constructor(
+        address _wbtcTokenAddress,
+        address _tTokenContract,
+        address _gmxV2btcVaultAddress,
+        address _uniswapbtcVaultAddress,
+        address _treasury
+    ) {
+        wbtcToken = Iwbtc(_wbtcTokenAddress);
+        tTokenContract = tToken(_tTokenContract);
+        gmxV2btcVault = GMXV2btc(_gmxV2btcVaultAddress);
+        uniswapbtcVault = uniswapbtc(_uniswapbtcVaultAddress);
+        addresses.treasury = _treasury;
+        config.gmxV2btcPercent = 50;
+        config.uniswapbtcPercent = 50;
+        config.performanceFee = 2000;
+    }
+
+    function deposit() external payable nonReentrant {
+        _deposit(msg.value);
+    }
+
+    function withdraw(uint256 tTokenAmount) external nonReentrant {
+        _withdraw(tTokenAmount);
+    }
+
+    function compoundFees() external nonReentrant {
+        _compoundFees();
+    }
+
+    function _deposit(uint256 amount) internal {
+        require(amount > 0, "Deposit amount must be greater than zero");
+        wbtcToken.deposit{value: amount}();
+        uint256 half = amount.div(2);
+        wbtcToken.approve(address(gmxV2btcVault), half);
+        gmxV2btcVault.deposit(half);
+        wbtcToken.approve(address(uniswapbtcVault), half);
+        uniswapbtcVault.deposit(half);
+        tTokenContract.mint(msg.sender, amount);
+        totalSupplied = totalSupplied.add(amount);
+        RewardUtil(rewardUtil).updateReward(msg.sender);
+        emit Deposit(msg.sender, amount, amount);
+    }
+
+    function _withdraw(uint256 tTokenAmount) internal {
+        _checkWithdraw(tTokenAmount);
+        uint256 btcAmount = calculatebtcAmount(tTokenAmount);
+        require(btcAmount <= address(this).balance, "Insufficient btc balance in contract");
+        gmxV2btcVault.withdraw(tTokenAmount.div(2));
+        uniswapbtcVault.withdraw(tTokenAmount.div(2));
+        tTokenContract.burn(msg.sender, tTokenAmount);
+        wbtcToken.withdraw(btcAmount);
+        (bool success, ) = msg.sender.call{value: btcAmount}("");
+        require(success, "btc transfer failed");
+        totalSupplied = totalSupplied.sub(btcAmount);
+        RewardUtil(rewardUtil).updateReward(msg.sender);
+        emit Withdraw(msg.sender, tTokenAmount, btcAmount);
+    }
+
+    function _compoundFees() internal {
+        require(block.timestamp >= lastCompoundTimestamp + 12 hours, "Minimum duration not met");
+        uint256 gmxV2btcBalanceBefore = gmxV2btcVault.balanceOf(address(this));
+        uint256 uniswapbtcBalanceBefore = uniswapbtcVault.balanceOf(address(this));
+        uint256 totalBalanceBefore = gmxV2btcBalanceBefore.add(uniswapbtcBalanceBefore);
+        gmxV2btcVault.withdrawGMX();
+        uniswapbtcVault.withdrawuniswap();
+        uint256 performanceFee = totalBalanceBefore.mul(config.performanceFee).div(10000);
+        uint256 treasuryFee = performanceFee.mul(20).div(100);
+        uint256 gmxV2btcFee = gmxV2btcVault.balanceOf(address(this));
+        uint256 uniswapbtcFee = uniswapbtcVault.balanceOf(address(this));
+        payable(addresses.treasury).transfer(treasuryFee);
+        uint256 totalBalanceAfter = gmxV2btcFee.add(uniswapbtcFee);
+        uint256 gmxV2btcFeeActualPercent = gmxV2btcFee.mul(100).div(totalBalanceAfter);
+        uint256 uniswapbtcFeeActualPercent = uniswapbtcFee.mul(100).div(totalBalanceAfter);
+        gmxV2btcVault.deposit();
+        uniswapbtcVault.deposit();
+        lastCompoundTimestamp = block.timestamp;
+        emit Compound(gmxV2btcFeeActualPercent, uniswapbtcFeeActualPercent);
+    }
+    
+    function calculatebtcAmount(uint256 tTokenAmount) public view returns (uint256) {
+        uint256 totalSupply = tTokenContract.totalSupply();
+        if (totalSupply == 0) return 0;
+        uint256 btcBalance = address(this).balance;
+        return tTokenAmount.mul(btcBalance).div(totalSupply);
+    }
+    
+    receive() external payable {}
 }
