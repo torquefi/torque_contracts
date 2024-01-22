37fdfa85114956df21b216b37d5ac80a253a716f
Mihir <aceinfotech.mihir@gmail.com>
2024-01-22 13:11:46 -0500

    updating BoostETH contract

--- contracts/TorqueProtocol/strategies/GMXV2ETH.sol
+++ contracts/TorqueProtocol/strategies/GMXV2ETH.sol
@@ -1,5 +1,5 @@
 // SPDX-License-Identifier: MIT
-pragma solidity ^0.8.20;
+pragma solidity ^0.8.19;
 
 //  _________  ________  ________  ________  ___  ___  _______
 // |\___   ___\\   __  \|\   __  \|\   __  \|\  \|\  \|\  ___ \
@@ -10,117 +10,89 @@ pragma solidity ^0.8.20;
 //        \|__|  \|_______|\|__|\|__|\|___| \__\|_______|\|_______|
 
 import "@openzeppelin/contracts/access/Ownable.sol";
+import "@openzeppelin/contracts/utils/math/SafeMath.sol";
 import "@openzeppelin/contracts/security/ReentrancyGuard.sol";
+import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
+import "@openzeppelin/contracts/token/ERC20/utils/SafeERC20.sol";
 
-import "@uniswap/v3-periphery/contracts/interfaces/ISwapRouter.sol";
+import "../../UniswapContracts/ISwapRouter.sol";
+import "../interfaces/IGMXExchangeRouter.sol";
 
-import "./../interfaces/IGMX.sol";
-import "./../interfaces/IExchangeRouter.sol";
-import "./../interfaces/IGMXV2ETH.sol";
+contract GMXV2ETH is Ownable, ReentrancyGuard {
+    using SafeMath for uint256;
+    using SafeERC20 for IERC20;
 
-contract GMXV2ETH is Ownable, ReentrancyGuard, IGMXV2ETH {
-    IERC20 public wethGMX;
+    IERC20 public weth;
     IERC20 public gmToken;
     IERC20 public usdcToken;
+
     address marketAddress;
     address depositVault;
     address withdrawalVault;
-    
-    IExchangeRouter public immutable gmxExchange;
-    ISwapRouter public immutable swapRouter;
-    
-    address private constant UNISWAP_V3_ROUTER = 0xc6962004f452be9203591991d15f6b388e09e8d0;
 
-    constructor(
-        address _weth,
-        address _gmxExchange,
-        address _gmToken,
-        address _usdcToken,
-        address _depositVault,
-        address _withdrawalVault
-    ) {
-        wethGMX = IERC20(_weth);
-        gmxExchange = IExchangeRouter(_gmxExchange);
-        gmToken = IERC20(_gmToken);
-        usdcToken = IERC20(_usdcToken);
-        depositVault = _depositVault;
-        withdrawalVault = _withdrawalVault;
+    IGMXExchangeRouter public exchangeRouter;
+    ISwapRouter public swapRouter;
+
+    constructor(address weth_, address gmToken_, address usdcToken_, address exchangeRouter_, address swapRouter_, address depositVault_, address withdrawalVault_){
+        weth = IERC20(weth_);
+        gmToken = IERC20(gmToken_);
+        usdcToken = IERC20(usdcToken_);
+        exchangeRouter = IGMXExchangeRouter(exchangeRouter_);
+        swapRouter = ISwapRouter(swapRouter_);
+        depositVault = depositVault_;
+        withdrawalVault = withdrawalVault_;
     }
 
-    function _deposit(uint256 _amount) internal returns (uint256 gmTokenAmount) {
-        gmxExchange.sendWnt(depositVault, _amount);
-        IExchangeRouter.CreateDepositParams memory params = createDepositParams();
-        wethGMX.safeTransferFrom(msg.sender, address(this), _amount);
-        wethGMX.approve(depositVault, _amount);
-        gmxExchange.createDeposit{ value: _amount }(params);
-        gmTokenAmount = gmToken.balanceOf(address(this));
-    }
-
-    function _withdraw(uint256 _amount) internal returns (uint256 initialWethBalance, uint256 usdcAmount, uint256 totalWethAmount) {
-        gmxExchange.sendTokens(address(gmToken), withdrawalVault, _amount);
-        IExchangeRouter.CreateWithdrawalParams memory params = createWithdrawParams();
-        gmToken.safeTransferFrom(msg.sender, address(this), _amount);
-        gmToken.approve(withdrawalVault, _amount);
-        gmxExchange.createWithdrawal(params);
-        initialWethBalance = wethGMX.balanceOf(address(this));
-        usdcAmount = usdcToken.balanceOf(address(this));
-        swapUSDCtoWETH(usdcAmount);
-        uint256 postSwapWethBalance = wethGMX.balanceOf(address(this));
-        totalWethAmount = postSwapWethBalance;
-        wethGMX.safeTransfer(msg.sender, totalWethAmount);
-    }
-
-    function _sendWnt(address _receiver, uint256 _amount) private {
-        gmxExchange.sendWnt(_receiver, _amount);
-    }
-
-    function _sendTokens(address _token, address _receiver, uint256 _amount) private {
-        gmxExchange.sendTokens(_token, _receiver, _amount);
-    }
-
-    function createDepositParams() internal view returns (IExchangeRouter.CreateDepositParams memory) {
-        IExchangeRouter.CreateDepositParams memory depositParams;
+    function deposit(uint256 _amount) external {
+        IGMXExchangeRouter.CreateDepositParams memory depositParams;
         depositParams.callbackContract = address(this);
         depositParams.callbackGasLimit = 0;
         depositParams.executionFee = 0;
-        depositParams.initialLongToken = address(wethGMX);
+        depositParams.initialLongToken = address(weth);
         depositParams.initialShortToken = address(usdcToken);
         depositParams.market = marketAddress;
         depositParams.shouldUnwrapNativeToken = true;
         depositParams.receiver = address(this);
         depositParams.minMarketTokens = 0;
-        return depositParams;
+        weth.safeTransferFrom(msg.sender, address(this), _amount);
+        weth.approve(depositVault, _amount);
+        exchangeRouter.createDeposit{value: _amount}(depositParams);
+        uint256 gmAmount = gmToken.balanceOf(address(this));
+        gmToken.safeTransfer(msg.sender, gmAmount);
     }
 
-    function createWithdrawParams() internal view returns (IExchangeRouter.CreateWithdrawalParams memory) {
-        IExchangeRouter.CreateWithdrawalParams memory withdrawParams;
+    function withdraw(uint256 _amount) external {
+        IGMXExchangeRouter.CreateWithdrawalParams memory withdrawParams;
         withdrawParams.callbackContract = address(this);
         withdrawParams.callbackGasLimit = 0;
         withdrawParams.executionFee = 0;
-        // withdrawParams.initialLongToken = address(weth);
-        // withdrawParams.initialShortToken = address(usdcToken);
         withdrawParams.market = marketAddress;
         withdrawParams.shouldUnwrapNativeToken = true;
         withdrawParams.receiver = address(this);
         withdrawParams.minLongTokenAmount = 0;
         withdrawParams.minShortTokenAmount = 0;
-        return withdrawParams;
+        gmToken.safeTransferFrom(msg.sender, address(this), _amount);
+        gmToken.approve(withdrawalVault, _amount);
+        exchangeRouter.createWithdrawal(withdrawParams);
+        uint256 usdcAmount = usdcToken.balanceOf(address(this));
+        swapUSDCtoWETH(usdcAmount);
+        uint256 wethAmount = weth.balanceOf(address(this));
+        weth.safeTransfer(msg.sender, wethAmount);
     }
 
     function swapUSDCtoWETH(uint256 usdcAmount) internal {
         usdcToken.approve(address(swapRouter), usdcAmount);
-        ISwapRouter.ExactInputSingleParams memory params =
+        ISwapRouter.ExactInputSingleParams memory params =  
             ISwapRouter.ExactInputSingleParams({
                 tokenIn: address(usdcToken),
-                tokenOut: address(wethGMX),
-                fee: 500, // Uniswap V3 0.05 ETH/USDC pool
+                tokenOut: address(weth),
+                fee: 0,
                 recipient: address(this),
                 deadline: block.timestamp,
                 amountIn: usdcAmount,
-                amountOutMinimum: 99,
+                amountOutMinimum: 0,
                 sqrtPriceLimitX96: 0
             });
-
         swapRouter.exactInputSingle(params);
     }
 }
--- contracts/TorqueProtocol/strategies/StargateETH.sol
+++ contracts/TorqueProtocol/strategies/StargateETH.sol
@@ -1,5 +1,5 @@
 // SPDX-License-Identifier: MIT
-pragma solidity ^0.8.20;
+pragma solidity ^0.8.9;
 
 //  _________  ________  ________  ________  ___  ___  _______
 // |\___   ___\\   __  \|\   __  \|\   __  \|\  \|\  \|\  ___ \
@@ -15,37 +15,63 @@ import "@openzeppelin/contracts/security/ReentrancyGuard.sol";
 import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
 import "@openzeppelin/contracts/token/ERC20/utils/SafeERC20.sol";
 
-import "./../interfaces/ISwapRouterV3.sol";
-import "./../interfaces/IStargateLPStaking.sol";
+import "../interfaces/IStargateLPStakingTime.sol";
+import "../interfaces/IStargateRouterETH.sol";
+import "../../StargateContracts/interfaces/IStargateRouter.sol";
+import "../interfaces/IWETH9.sol";
 
-contract StargateETH is Ownable, ReentrancyGuard {
+import "../../UniswapContracts/ISwapRouter.sol";
+
+contract StargateETH is Ownable, ReentrancyGuard{
     using SafeMath for uint256;
     using SafeERC20 for IERC20;
 
-    IERC20 public immutable wethSTG;
-    IStargateLPStaking lpStaking;
-
-    mapping(address => uint256) public addressToPid;
-
-    constructor(address _weth, address _stargateStakingAddress, address initialOwner) Ownable(initialOwner) {
-        wethSTG = IERC20(_weth);
-        lpStaking = IStargateLPStaking(_stargateStakingAddress);
+    IWETH9 public weth;
+    IERC20 public wethSTG;
+    IERC20 public arbToken;
+    IStargateRouterETH public routerETH;
+    IStargateLPStakingTime public lpStakingTime;
+    IStargateRouter public router;
+    ISwapRouter public swapRouter;
+    
+    constructor(address payable weth_, address wethSTG_, address arbToken_, address routerETH_, address lpStakingTime_, address router_, address swapRouter_){
+        weth = IWETH9(weth_);
+        wethSTG = IERC20(wethSTG_);
+        arbToken = IERC20(arbToken_);
+        routerETH = IStargateRouterETH(routerETH_);
+        lpStakingTime = IStargateLPStakingTime(lpStakingTime_);
+        router = IStargateRouter(router_);
+        swapRouter = ISwapRouter(swapRouter_);
     }
 
-    function _deposit(address _token, uint256 _amount) internal {
-        uint256 pid = addressToPid[_token];
-        wethSTG.safeTransferFrom(msg.sender, address(this), _amount);
-        wethSTG.approve(address(lpStaking), _amount);
-        lpStaking.deposit(pid, _amount);
+    function deposit(uint256 _amount) payable external {
+        weth.transferFrom(msg.sender, address(this), _amount);
+        weth.withdraw(_amount);
+        routerETH.addLiquidity{value: _amount}();
+        wethSTG.approve(address(lpStakingTime), _amount);
+        lpStakingTime.deposit(2, _amount);
     }
 
-    function _withdraw(address _token, uint256 _amount) internal {
-        uint256 pid = addressToPid[_token];
-        lpStaking.withdraw(pid, _amount);
-        wethSTG.safeTransfer(msg.sender, _amount);
+    function withdraw(uint256 _amount) external {
+        lpStakingTime.withdraw(2, _amount);
+        wethSTG.approve(address(router), _amount);
+        router.instantRedeemLocal(13, _amount, address(this));
+        weth.approve(address(msg.sender), _amount);
     }
 
-    function setPid(address _token, uint256 _pid) public onlyOwner {
-        addressToPid[_token] = _pid;
+    function swapARBtoWETH(uint256 arbAmount) internal {
+        arbToken.approve(address(swapRouter), arbAmount);
+        ISwapRouter.ExactInputSingleParams memory params =
+            ISwapRouter.ExactInputSingleParams({
+                tokenIn: address(arbToken),
+                tokenOut: address(weth),
+                fee: 0,
+                recipient: address(this),
+                deadline: block.timestamp,
+                amountIn: arbAmount,
+                amountOutMinimum:0,
+                sqrtPriceLimitX96: 0
+            });
+        swapRouter.exactInputSingle(params);
     }
 }
