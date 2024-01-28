bb2644828b5053f74e019a4c99952116b7300ebf
Pranjal Surana <99ps.work@gmail.com>
2024-01-29 00:09:23 +0530

    More changes

--- contracts/TorqueProtocol/strategies/GMXV2BTC.sol
+++ contracts/TorqueProtocol/strategies/GMXV2BTC.sol
@@ -15,23 +15,24 @@ import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
 import "@uniswap/v3-periphery/contracts/interfaces/ISwapRouter.sol";
 
 import "./../interfaces/IGMX.sol";
-import "./../interfaces/IExchangeRouter.sol";
-import "./../interfaces/IGMXV2ETH.sol";
+import "../interfaces/IGMXExchangeRouter.sol";
 
-contract GMXV2BTC is Ownable, ReentrancyGuard, IGMXV2ETH {
+contract GMXV2BTC is Ownable, ReentrancyGuard {
     IERC20 public wbtcGMX;
     IERC20 public gmToken;
     IERC20 public usdcToken;
+    IERC20 public arbToken;
+
     address marketAddress;
     address depositVault;
     address withdrawalVault;
     address router;
 
     uint256 public depositedBTCAmount;
-
     uint256 public executionFee; 
+    uint256 minUSDCAmount = 0;
     
-    IExchangeRouter public immutable gmxExchange;
+    IGMXExchangeRouter public immutable gmxExchange;
     ISwapRouter public immutable swapRouter;
     
     address private constant UNISWAP_V3_ROUTER = 0x2f5e87C9312fa29aed5c179E456625D79015299c;
@@ -45,7 +46,7 @@ contract GMXV2BTC is Ownable, ReentrancyGuard, IGMXV2ETH {
         address swapRouter_, 
         address depositVault_, 
         address withdrawalVault_, 
-        address router_){
+        address router_) Ownable(msg.sender) {
             wbtcGMX = IERC20(_wbtc);
             gmToken = IERC20(gmToken_);
             usdcToken = IERC20(usdcToken_);
@@ -83,7 +84,7 @@ contract GMXV2BTC is Ownable, ReentrancyGuard, IGMXV2ETH {
             swapUSDCtoWETH(usdcAmount);
         }
         uint256 btcAmount = wbtcGMX.balanceOf(address(this));
-        wbtcGMX.safeTransfer(msg.sender, btcAmount);
+        wbtcGMX.transfer(msg.sender, btcAmount);
     }
 
     function _sendWnt(address _receiver, uint256 _amount) private {
@@ -94,8 +95,8 @@ contract GMXV2BTC is Ownable, ReentrancyGuard, IGMXV2ETH {
         gmxExchange.sendTokens(_token, _receiver, _amount);
     }
 
-    function createDepositParams() internal view returns (IExchangeRouter.CreateDepositParams memory) {
-        IExchangeRouter.CreateDepositParams memory depositParams;
+    function createDepositParams() internal view returns (IGMXExchangeRouter.CreateDepositParams memory) {
+        IGMXExchangeRouter.CreateDepositParams memory depositParams;
         depositParams.callbackContract = address(this);
         depositParams.callbackGasLimit = 0;
         depositParams.executionFee = executionFee;
@@ -108,8 +109,8 @@ contract GMXV2BTC is Ownable, ReentrancyGuard, IGMXV2ETH {
         return depositParams;
     }
 
-    function createWithdrawParams() internal view returns (IExchangeRouter.CreateWithdrawalParams memory) {
-        IExchangeRouter.CreateWithdrawalParams memory withdrawParams;
+    function createWithdrawParams() internal view returns (IGMXExchangeRouter.CreateWithdrawalParams memory) {
+        IGMXExchangeRouter.CreateWithdrawalParams memory withdrawParams;
         withdrawParams.callbackContract = address(this);
         withdrawParams.callbackGasLimit = 0;
         withdrawParams.executionFee = executionFee;
@@ -140,6 +141,22 @@ contract GMXV2BTC is Ownable, ReentrancyGuard, IGMXV2ETH {
 
     function updateExecutionFee(uint256 _executionFee) public onlyOwner{
         executionFee = _executionFee;
+    }
+
+    function swapUSDCtoWETH(uint256 usdcAmount) internal {
+        usdcToken.approve(address(swapRouter), usdcAmount);
+        ISwapRouter.ExactInputSingleParams memory params =  
+            ISwapRouter.ExactInputSingleParams({
+                tokenIn: address(usdcToken),
+                tokenOut: address(wbtcGMX),
+                fee: 0,
+                recipient: address(this),
+                deadline: block.timestamp,
+                amountIn: usdcAmount,
+                amountOutMinimum: 0,
+                sqrtPriceLimitX96: 0
+            });
+        swapRouter.exactInputSingle(params);
     }
 
     receive() external payable{}
